c87cf941f6626a4ab14ab32790d5810c281d8882
Swyter <690187+Swyter@users.noreply.github.com>
2012-04-28 15:45:30 +0200

    *Quick one: Repaired broken Data folder compilation.

--- Data/build_data.bat
+++ Data/build_data.bat
@@ -15,7 +15,7 @@ cd ..
 set CD="!CD!"
 
 :: setup our python and specify what folders are included in the search path for scripts
-set PATH=!CD:~1,-1!\Python
+set PATH=!CD:~1,-1!\Builder\Python
 set PYTHONPATH=%PYTHONPATH%;!CD:~1,-1!\Process;!CD:~1,-1!\Data;!CD:~1,-1!
 
 :: the -B param overides the pyc/pyo bytecode generation, so there's no need for deleting them later :)
