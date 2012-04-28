84d42a483d845f10fe204b3d42ad2f929915eab2
Swyter <690187+Swyter@users.noreply.github.com>
2012-04-29 00:22:57 +0200

    *It works and it's way cleaner now, I've a problem with variable scope tho...  If someboby can take an eye to process_swybuildmix and tell my with does not pass the vars between local and global... :)

--- Process/process__swybuildmixb.py
+++ Process/process__swybuildmixb.py
@@ -1,12 +1,50 @@
 from time import time
+import sys
 start = time()
 
-import process_scripts
-import process_mission_tmps
-import process_game_menus
-import process_simple_triggers
-import process_dialogs
-import process_global_variables_unused
-import process_postfx
+def handler(test):
+  exc_type, exc_value, exc_traceback = sys.exc_info()
+  print("->%s"%(exc_value))
+  #print("'%s'\r\n'%s'"%(test,exc_value))
+  if test and test!=exc_value:
+      print("!({0})".format(exc_value))
+      i=exc_value
+# import process_scripts
+# import process_mission_tmps
+# import process_game_menus
+# import process_simple_triggers
+# import process_dialogs
+# import process_global_variables_unused
+# import process_postfx
+
+try:
+ import process_scripts
+except NameError:
+ i=handler(None)
+ 
+try:
+ import process_game_menus
+except NameError:
+ i=handler(None)
+ 
+try:
+ import process_simple_triggers
+except NameError:
+ i=handler(None)
+ 
+try:
+ import process_dialogs
+except NameError:
+ i=handler(None)
+ 
+try:
+ import process_global_variables_unused
+except NameError:
+ i=handler(None)
+ 
+try:
+ import process_postfx
+except NameError:
+ i=handler(None)
 
 print("It took %ss"%(time()-start))
--- build_module.bat
+++ build_module.bat
@@ -18,38 +18,38 @@ set PATH=!CD:~1,-1!\Builder\Python
 set PYTHONPATH=%PYTHONPATH%;!CD:~1,-1!\ID;!CD:~1,-1!\Header;!CD:~1,-1!\Process;!CD:~1,-1!
 
 :: the -B param overides the pyc/pyo bytecode generation, so there's no need for deleting them later :)
-::python -B  -OO Process/process__swybuildmix.py
-::python -B  -OO Process/process__swybuildmixb.py
+python -B  -OO Process/process__swybuildmix.py
+python -B  -OO Process/process__swybuildmixb.py
 
-python -B Process/process_init.py
-python -B Process/process_global_variables.py
-python -B Process/process_strings.py
-python -B Process/process_skills.py
-python -B Process/process_music.py
-python -B Process/process_animations.py
-python -B Process/process_meshes.py
-python -B Process/process_sounds.py
-python -B Process/process_skins.py
-python -B Process/process_map_icons.py
-python -B Process/process_factions.py
-python -B Process/process_items.py
-python -B Process/process_scenes.py
-python -B Process/process_troops.py
-python -B Process/process_particle_sys.py
-python -B Process/process_scene_props.py
-python -B Process/process_tableau_materials.py
-python -B Process/process_presentations.py
-python -B Process/process_party_tmps.py
-python -B Process/process_parties.py
-python -B Process/process_quests.py
-python -B Process/process_info_pages.py
-python -B Process/process_scripts.py
-python -B Process/process_mission_tmps.py
-python -B Process/process_game_menus.py
-python -B Process/process_simple_triggers.py
-python -B Process/process_dialogs.py
-python -B Process/process_global_variables_unused.py
-python -B Process/process_postfx.py
+REM python -B Process/process_init.py
+REM python -B Process/process_global_variables.py
+REM python -B Process/process_strings.py
+REM python -B Process/process_skills.py
+REM python -B Process/process_music.py
+REM python -B Process/process_animations.py
+REM python -B Process/process_meshes.py
+REM python -B Process/process_sounds.py
+REM python -B Process/process_skins.py
+REM python -B Process/process_map_icons.py
+REM python -B Process/process_factions.py
+REM python -B Process/process_items.py
+REM python -B Process/process_scenes.py
+REM python -B Process/process_troops.py
+REM python -B Process/process_particle_sys.py
+REM python -B Process/process_scene_props.py
+REM python -B Process/process_tableau_materials.py
+REM python -B Process/process_presentations.py
+REM python -B Process/process_party_tmps.py
+REM python -B Process/process_parties.py
+REM python -B Process/process_quests.py
+REM python -B Process/process_info_pages.py
+REM python -B Process/process_scripts.py
+REM python -B Process/process_mission_tmps.py
+REM python -B Process/process_game_menus.py
+REM python -B Process/process_simple_triggers.py
+REM python -B Process/process_dialogs.py
+REM python -B Process/process_global_variables_unused.py
+REM python -B Process/process_postfx.py
 
 title [X] swysdk -- finished
 echo ______________________________
