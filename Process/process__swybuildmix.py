f9d163f9acfe049d375860b50161c5366aec836d
Swyter <690187+Swyter@users.noreply.github.com>
2012-04-29 11:33:23 +0200

    *Now every error is shown only once. Cleaner syntax.  Managed to solve my problem with the convoluted world of python variables scope.
     Looks like you have to indicate it with a 'global whatever', beforehand.

--- Process/process__swybuildmix.py
+++ Process/process__swybuildmix.py
@@ -1,124 +1,124 @@
 from time import time
 import sys
 start = time()
+G = ""
 
-def handler(test):
+def handler():
+  global G
   exc_type, exc_value, exc_traceback = sys.exc_info()
-  print("->%s"%(exc_value))
-  #print("'%s'\r\n'%s'"%(test,exc_value))
-  if test and test!=exc_value:
-      print("!({0})".format(exc_value))
-      i=exc_value
+  if str(G)!=str(exc_value):
+    print("!({0})".format(exc_value))
+  G=exc_value
     
 try:
  import process_init
 except NameError:
- i=handler(None)
+ handler()
 
 try:
  import process_global_variables
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_strings
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_skills
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_music
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_animations
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_meshes
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_sounds
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_skins
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_map_icons
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_factions
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_items
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_scenes
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_troops
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_particle_sys
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_scene_props
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_tableau_materials
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_presentations
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_party_tmps
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_parties
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_quests
 except NameError:
- i=handler(i)
+ handler()
 
 try:
  import process_info_pages
 except NameError:
- i=handler(i)
+ handler()
  
 # don't ask me why, but there's a sort of variable overwrite in process_scripts that corrupts some objects...
 # it goes nuts and starts throwing warnings about "unable to find anim_***" that's why we have to divide the processing in two separate python batches. :/
--- Process/process__swybuildmixb.py
+++ Process/process__swybuildmixb.py
@@ -1,50 +1,43 @@
 from time import time
 import sys
 start = time()
+G = ""
 
-def handler(test):
+def handler():
+  global G
   exc_type, exc_value, exc_traceback = sys.exc_info()
-  print("->%s"%(exc_value))
-  #print("'%s'\r\n'%s'"%(test,exc_value))
-  if test and test!=exc_value:
-      print("!({0})".format(exc_value))
-      i=exc_value
-# import process_scripts
-# import process_mission_tmps
-# import process_game_menus
-# import process_simple_triggers
-# import process_dialogs
-# import process_global_variables_unused
-# import process_postfx
+  if str(G)!=str(exc_value):
+    print("!({0})".format(exc_value))
+  G=exc_value
 
 try:
  import process_scripts
 except NameError:
- i=handler(None)
+ handler()
  
 try:
  import process_game_menus
 except NameError:
- i=handler(None)
+ handler()
  
 try:
  import process_simple_triggers
 except NameError:
- i=handler(None)
+ handler()
  
 try:
  import process_dialogs
 except NameError:
- i=handler(None)
+ handler()
  
 try:
  import process_global_variables_unused
 except NameError:
- i=handler(None)
+ handler()
  
 try:
  import process_postfx
 except NameError:
- i=handler(None)
+ handler()
 
 print("It took %ss"%(time()-start))
